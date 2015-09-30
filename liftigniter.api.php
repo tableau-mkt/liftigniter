a364be4228498e31c21b13a326deb0b702e4a628
Josh Lind <jlind@tableausoftware.com>
2015-09-30 14:23:10 -0700

    Issue #2577939: Add workable meta data

--- liftigniter.api.php
+++ liftigniter.api.php
@@ -19,3 +19,32 @@ function hook_liftigniter_templates_alter(&$locations) {
   // Add your module to the front of the list of template locations.
   array_unshift($locations, drupal_get_path('module', 'my_module') . '/templates');
 }
+
+
+/**
+ * Declare entity meta data properties for output.
+ *
+ * @return array
+ *   Values will be merged into entity data searched for and output.
+ */
+function hook_liftigniter_meta() {
+  return array(
+    'created';
+  );
+}
+
+
+/**
+ * Alter or add to meta data output.
+ *
+ * @param array &$data
+ */
+function hook_liftigniter_meta_alter(&$data) {
+  // Add something.
+  $menu_route = menu_get_active_trail();
+  $data['menu-parent'] = $menu_route[1]['title'];
+
+  // Change something.
+  $bundle = $data['bundle'];
+  $data['bundle'] = ($bundle === 'my_type') ? 'My Fancy Type' : $bundle;
+}
